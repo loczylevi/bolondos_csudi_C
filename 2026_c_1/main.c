6611a36f28d2c1a48250b240ea30f5c13386a33e
Lóczy Levente Attila <71691777+loczylevi@users.noreply.github.com>
2026-08-08 21:02:27 +0200

    Create main.c

---
+++ 2026_c_1/main.c
@@ -0,0 +1,19 @@
+
+
+#include <stdio.h>
+
+
+int main(){
+
+    int alma = 2;
+    int korte;
+
+    printf("szia vilmoyxycxys\n");
+    printf("%d %d", 2*7, 5*6);
+    printf("\nalma: %d %d",alma, korte );
+    
+    return 0;
+}
+
+
+
